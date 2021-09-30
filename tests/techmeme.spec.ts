6ccfb1ae0f70f01749258cfc2f5a1d1bad1e5d77
Kushal <kushal@gmx.com>
2021-09-30 17:16:39 -0600

    update techmeme

--- tests/techmeme.spec.ts
+++ tests/techmeme.spec.ts
@@ -1,5 +1,23 @@
 import { test, expect } from '@playwright/test';
 
 test('test', async ({ page }) => {
+
+  // Go to https://techmeme.com/
   await page.goto('https://techmeme.com/');
+
+  // Click text=River
+  await page.click('text=River');
+  await expect(page).toHaveURL('https://techmeme.com/river');
+
+  // Click text=About
+  await page.click('text=About');
+  await expect(page).toHaveURL('https://techmeme.com/about');
+
+  // Click :nth-match(:text("memeorandum"), 3)
+  await page.click(':nth-match(:text("memeorandum"), 3)');
+  await expect(page).toHaveURL('https://www.memeorandum.com/');
+
+  // Go to https://www.memeorandum.com/river
+  await page.goto('https://www.memeorandum.com/river');
+
 });
